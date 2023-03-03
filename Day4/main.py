28cebc0bb74612693c9825ebe8134a581425c49b
Karthik Sankar <49610482+emergenitro@users.noreply.github.com>
2023-03-04 07:28:30 +0800

    Revert "Added Day 4 Part 2"

--- Day4/main.py
+++ Day4/main.py
@@ -1,32 +1,21 @@
 import os
 
 count = 0
-# with open(os.getcwd() + "\input.txt", "r") as file:
-#     lines = file.readlines()
-#     for line in lines:
-#         line = line.replace("\n", "").split(",")
-#         line[0], line[1] = line[0].split("-"), line[1].split("-")
-#         result = all(
-#             i in [i for i in range(int(line[0][0]),
-#                                    int(line[0][1]) + 1)]
-#             for i in [i for i in range(int(line[1][0]),
-#                                        int(line[1][1]) + 1)])
-#         result1 = all(
-#             i in [i for i in range(int(line[1][0]),
-#                                    int(line[1][1]) + 1)]
-#             for i in [i for i in range(int(line[0][0]),
-#                                        int(line[0][1]) + 1)])
-#         if result or result1:
-#             count += 1
-# print(count)
-
-# Part 2
 with open(os.getcwd() + "\input.txt", "r") as file:
     lines = file.readlines()
     for line in lines:
         line = line.replace("\n", "").split(",")
         line[0], line[1] = line[0].split("-"), line[1].split("-")
-        firstSection = [i for i in range(int(line[0][0]), int(line[0][1]) + 1)]
-        secondSection = [i for i in range(int(line[1][0]), int(line[1][1]) + 1)]
-        if (set(firstSection) & set(secondSection)): count += 1
+        result = all(
+            i in [i for i in range(int(line[0][0]),
+                                   int(line[0][1]) + 1)]
+            for i in [i for i in range(int(line[1][0]),
+                                       int(line[1][1]) + 1)])
+        result1 = all(
+            i in [i for i in range(int(line[1][0]),
+                                   int(line[1][1]) + 1)]
+            for i in [i for i in range(int(line[0][0]),
+                                       int(line[0][1]) + 1)])
+        if result or result1:
+            count += 1
 print(count)
