3f21177d272ca39b194f615d3bfa33399c20c469
Restyled.io <commits@restyled.io>
2023-03-03 13:07:56 +0000

    Restyled by autopep8

--- Day4/main.py
+++ Day4/main.py
@@ -5,7 +5,10 @@ with open(os.getcwd() + "\input.txt", "r") as file:
     for line in lines:
         line = line.replace("\n", "").split(",")
         line[0], line[1] = line[0].split("-"), line[1].split("-")
-        result = all(i in [i for i in range(int(line[0][0]), int(line[0][1]) + 1)] for i in [i for i in range(int(line[1][0]), int(line[1][1]) + 1)])
-        result1 = all(i in [i for i in range(int(line[1][0]), int(line[1][1]) + 1)] for i in [i for i in range(int(line[0][0]), int(line[0][1]) + 1)])
-        if result or result1: count += 1
+        result = all(i in [i for i in range(int(line[0][0]), int(line[0][1]) + 1)]
+                     for i in [i for i in range(int(line[1][0]), int(line[1][1]) + 1)])
+        result1 = all(i in [i for i in range(int(line[1][0]), int(line[1][1]) + 1)]
+                      for i in [i for i in range(int(line[0][0]), int(line[0][1]) + 1)])
+        if result or result1:
+            count += 1
 print(count)
